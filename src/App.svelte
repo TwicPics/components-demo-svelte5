7c577df79054675f54e6951d2453e4257db8272d
Miguel Beignon <mbgspcii@gmail.com>
2023-07-24 16:48:22 +0200

    Migration from svelte3.

---
+++ src/App.svelte
@@ -0,0 +1,66 @@
+<script>
+  import router from 'page';
+  import Home from './lib/Home.svelte';
+  import Anchor from './lib/TwicAnchor.svelte';
+  import ArtDirections from './lib/TwicArtDirections.svelte';
+  import BasicGrid from './lib/TwicBasicGrid.svelte';
+  import Bot from './lib/TwicBot.svelte';
+  import BulkLoading from './lib/TwicBulkLoading.svelte';
+  import ColorBlindness from './lib/TwicColorBlindness.svelte';
+  import Eager from './lib/TwicEager.svelte';
+  import FlipCards from './lib/TwicFlipCards.svelte';
+  import Focus from './lib/TwicFocus.svelte';
+  import GridAndZoom from './lib/TwicGridAndZoom.svelte';
+  import Intrinsic from './lib/TwicIntrinsic.svelte';
+  import Hero from './lib/TwicHero.svelte';
+  import Modal from './lib/TwicModal.svelte';
+  import Mode from './lib/TwicMode.svelte';
+  import Placeholder from './lib/TwicPlaceholder.svelte';
+  import Position from './lib/TwicPosition.svelte';
+  import PreTransform from './lib/TwicPreTransform.svelte';
+  import Ratio from './lib/TwicRatio.svelte';
+  import Refit from './lib/TwicRefit.svelte';
+  import Slider from './lib/TwicSlider.svelte';
+  import Slideshow from './lib/TwicSlideshow.svelte';
+  import StyleDriven from './lib/TwicStyleDriven.svelte';
+  import PageTemplating from './lib/TwicPageTemplating.svelte';
+  import State from './lib/TwicState.svelte';
+  import Transition from './lib/TwicTransition.svelte';
+  import ShortVideos from './lib/TwicShortVideos.svelte';
+  import Zoom from './lib/TwicZoom.svelte';
+
+  let page;
+
+  router('/', () => (page = Home));
+  router('/anchor', () => (page = Anchor));
+  router('/art-directions', () => (page = ArtDirections));
+  router('/basic-grid', () => (page = BasicGrid));
+  router('/bot', () => (page = Bot));
+  router('/bulk-loading', () => (page = BulkLoading));
+  router('/color-blindness', () => (page = ColorBlindness));
+  router('/eager', () => (page = Eager));
+  router('/flip-cards', () => (page = FlipCards));
+  router('/focus', () => (page = Focus));
+  router('/grid-and-zoom', () => (page = GridAndZoom));
+  router('/hero', () => (page = Hero));
+  router('/intrinsic', () => (page = Intrinsic));
+  router('/modal', () => (page = Modal));
+  router('/mode', () => (page = Mode));
+  router('/page-templating', () => (page = PageTemplating));
+  router('/placeholder', () => (page = Placeholder));
+  router('/position', () => (page = Position));
+  router('/pre-transform', () => (page = PreTransform));
+  router('/ratio', () => (page = Ratio));
+  router('/refit', () => (page = Refit));
+  router('/slider', () => (page = Slider));
+  router('/slideshow', () => (page = Slideshow));
+  router('/state', () => (page = State));
+  router('/style-driven', () => (page = StyleDriven));
+  router('/transition', () => (page = Transition));
+  router('/short-videos', () => (page = ShortVideos));
+  router('/zoom', () => (page = Zoom));
+
+  router.start();
+</script>
+
+<svelte:component this={page} />
---
+++ src/lib/TwicStyleDriven.svelte
@@ -0,0 +1,183 @@
+<script>
+  import TwicAbstract from './components/demo-wrapper/TwicAbstract.svelte';
+  import TwicWrapper from './components/demo-wrapper/TwicWrapper.svelte';
+  import Code from './components/Code.svelte';
+  import { TwicImg } from '@twicpics/components/svelte3';
+
+  const imgUrl = `components/horse.jpg`;
+</script>
+
+<div id="twic-style-driven-container">
+  <TwicWrapper filename="src/lib/TwicStyleDriven.svelte">
+    <TwicAbstract title="CSS style driven">
+      <p>
+        You can set up the TwicPics components using pure CSS and the power of <a
+          href="https://www.twicpics.com/docs/components/sveltekit#css-variables"
+          target="_blank"
+          rel="noreferrer">CSS variable</a
+        >.
+      </p>
+    </TwicAbstract>
+    <p>
+      <b>1.</b> Set up aspect ratio with <Code>--twic-ratio</Code> css variable.
+    </p>
+    <div class="twic-grid">
+      <div class="twic-item square">
+        <TwicImg src={imgUrl} />
+        <span>
+          class
+          <Code>square</Code>
+        </span>
+      </div>
+      <div class="twic-item portrait">
+        <TwicImg src={imgUrl} />
+        <span>
+          class
+          <Code>portrait</Code>
+        </span>
+      </div>
+      <div class="twic-item landscape">
+        <TwicImg src={imgUrl} />
+        <span>
+          class
+          <Code>landscape</Code>
+        </span>
+      </div>
+    </div>
+    <p>
+      <b>2.</b> Set up <a href="/zoom" target="_blank" rel="noreferrer">zoom</a> value with <Code
+        >--twic-zoom</Code
+      > css variable.
+    </p>
+    <div class="twic-grid">
+      <div class="twic-item zoom-sm">
+        <TwicImg src={imgUrl} zoom="css" />
+        <span>
+          class
+          <Code>zoom-sm</Code>
+        </span>
+      </div>
+      <div class="twic-item zoom-md">
+        <TwicImg src={imgUrl} zoom="css" />
+        <span>
+          class
+          <Code>zoom-md</Code>
+        </span>
+      </div>
+      <div class="twic-item zoom-lg">
+        <TwicImg src={imgUrl} zoom="css" />
+        <span>
+          class
+          <Code>zoom-lg</Code>
+        </span>
+      </div>
+    </div>
+    <p>
+      <b>3.</b> Class combination: you can set up multiples props as <Code>object-fit</Code> and
+      <Code>position</Code> with
+      <Code>--twic-mode</Code> and
+      <Code>twic-position</Code>
+    </p>
+    <br />
+    <div class="twic-grid">
+      <div class="twic-item contain left">
+        <TwicImg src={imgUrl} ratio="16/9" />
+        <span>
+          class
+          <Code>contain</Code>& class
+          <Code>left</Code>
+        </span>
+      </div>
+      <div class="twic-item contain right">
+        <TwicImg src={imgUrl} ratio="16/9" />
+        <span>
+          class
+          <Code>contain</Code>& class
+          <Code>right</Code>
+        </span>
+      </div>
+      <div class="twic-item cover">
+        <TwicImg src={imgUrl} focus="50px100p" ratio="16/9" />
+        <span>
+          class
+          <Code>cover</Code>
+        </span>
+      </div>
+    </div>
+    <p>
+      <b>4.</b> Other classical settings (here with
+      <Code>width</Code>)
+    </p>
+    <br />
+    <div class="twic-grid">
+      <div class="twic-item lg">
+        <TwicImg src={imgUrl} />
+        <span>
+          class
+          <Code>lg</Code>
+        </span>
+      </div>
+      <div class="twic-item md">
+        <TwicImg src={imgUrl} />
+        <span>
+          class
+          <Code>md</Code>
+        </span>
+      </div>
+      <div class="twic-item sm">
+        <TwicImg src={imgUrl} />
+        <span>
+          class
+          <Code>sm</Code>
+        </span>
+      </div>
+    </div>
+  </TwicWrapper>
+</div>
+
+<style lang="scss">
+  #twic-style-driven-container {
+    .twic-grid {
+      margin-bottom: 60px;
+    }
+    .landscape {
+      --twic-ratio: calc(4 / 3);
+    }
+    .portrait {
+      --twic-ratio: calc(3 / 4);
+    }
+    .square {
+      --twic-ratio: calc(1);
+    }
+    .contain {
+      --twic-mode: contain;
+    }
+    .cover {
+      --twic-mode: cover;
+    }
+    .left {
+      --twic-position: left;
+    }
+    .right {
+      --twic-position: right;
+    }
+    .lg {
+      width: 300px;
+    }
+    .md {
+      width: 150px;
+    }
+    .sm {
+      width: 100px;
+    }
+    .zoom-sm {
+      --twic-zoom: 1.5;
+    }
+    .zoom-md {
+      --twic-zoom: 2;
+    }
+    .zoom-lg {
+      --twic-zoom: 3;
+    }
+  }
+</style>
